645d46fdc4691bf05450b599ff837034b1b9b84a
raquelmargar <r.marting.2025@alumnos.urjc.es>
2026-02-11 18:36:07 +0100

    exercise 6 simple exercises

--- P04/simple exercises/exercise 6.py
+++ P04/simple exercises/exercise 6.py
@@ -1,1 +1,11 @@
-def
+def is_even(number):
+    if number % 2 == 0:
+        return "True"
+    else:
+        return "False"
+
+print("is_even(4) = ", is_even(4))
+print("is_even(7) = ", is_even(7))
+print("is_even(0) = ", is_even(0))
+print("is_even(-3) = ", is_even(-3))
+print("is_even(10) = ", is_even(10))
