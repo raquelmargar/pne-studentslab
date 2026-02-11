b2e52486315fd9804a019e04d71c1777ff89d7ad
raquelmargar <r.marting.2025@alumnos.urjc.es>
2026-02-11 18:45:44 +0100

    exercise 6 simple exercises

--- P04/simple exercises/exercise 6.py
+++ P04/simple exercises/exercise 6.py
@@ -9,3 +9,15 @@ print("is_even(7) = ", is_even(7))
 print("is_even(0) = ", is_even(0))
 print("is_even(-3) = ", is_even(-3))
 print("is_even(10) = ", is_even(10))
+
+def classify_triangle(a, b, c):
+    if a == b == c:
+        return "equilateral"
+    elif a == b or b == c or c == a:
+        return "isosceles"
+    else:
+        return "scalene"
+
+print("classify triangle(5, 5, 5) = ", classify_triangle(5, 5, 5))
+print("classify triangle(3, 3, 4) = ", classify_triangle(3, 3, 4))
+print("classify triangle(3, 4, 5) = ", classify_triangle(3, 4, 5))
