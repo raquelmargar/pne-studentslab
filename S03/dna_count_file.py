4c0e1fe65f6b1e4a7f883cd81565c9a5205b5c2a
raquelmargar <r.marting.2025@alumnos.urjc.es>
2026-02-11 18:10:10 +0100

    exercise 3 simple exercises

--- S03/dna_count_file.py
+++ S03/dna_count_file.py
@@ -1,3 +1,5 @@
+from dna_count import count_bases
+
 #option 1
 f = open("dan.txt","r")
 lines = f.readlines()
@@ -7,29 +9,4 @@ f.close()
 with open("dna.txt","r") as f:
     linesz = f.readlines()
 
-def length_dna(sequence):
-    return len(sequence)
-
-def count_bases(sequence):
-    count_A = count_C = count_T = count_G = 0
-    for i in sequence:
-        if i == "A":
-            count_A += 1
-        elif i == "C":
-            count_C += 1
-        elif i == "T":
-            count_T += 1
-        else:
-            count_G += 1
-    return(count_A, count_C, count_T, count_G)
-
-sequence = input("Enter a DNA sequence: ")
-length = length_dna(sequence)
-a, c, t, g = count_bases(sequence)
-
-print("Total length: ", length)
-print("A: ", a)
-print("C: ", c)
-print("T: ", t)
-print("G: ", g)
-
+count_bases("")
