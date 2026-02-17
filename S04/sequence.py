ef6af43ed3392121edb23636d6f18bd056b89929
raquelmargar <r.marting.2025@alumnos.urjc.es>
2026-02-17 16:29:40 +0100

    exercise 5 Ccount bases

---
+++ S04/sequence.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+FILENAME = "sequences/ADA.txt"
+
+file_contents = Path(FILENAME).read_text()
+a = file_contents.split("\n")
+body = a[1::]
+b = "".join(body)
+print("The total number of bases is:", len(b))
