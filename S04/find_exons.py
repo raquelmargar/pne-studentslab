7517f09824b3c67b736051361af80a3ed869d51a
raquelmargar <r.marting.2025@alumnos.urjc.es>
2026-02-18 17:31:10 +0100

    exercise 6 Ccount bases

--- S04/find_exons.py
+++ S04/find_exons.py
@@ -5,7 +5,24 @@ FILENAME = "sequences/ADA.txt"
 file_contents = Path(FILENAME).read_text()
 a = file_contents.split("\n")
 body = a[1::]
-b = "".join(body)
+gene_seq = "".join(body)
 
-exon = b.find("GCTGGCCCCAGGGAAAGCCGAGCGGCCACCGAGCCGGCAGAGACCCACCGAGCGGCGGCGGAGGGAGCAGCGCCGGGGCGCACGAGGGCACCATGGCCCAGACGCCCGCCTTCGACAAGCCCAAA")
-print(exon)
+MAX_COORD = 44652852
+EXON_FILE = "sequences/ADA_EXONS.txt"
+exon_contents = Path(EXON_FILE).read_text()
+exons = exon_contents.split(">")
+#tienes que separar cada exon con > y luego quitarle esa primera linea,que es el header
+print("Exon     | Long.  | Start            | End")
+print("-----------------------------------------------------")
+
+n = 1
+
+for exon in exons:
+    index = gene_seq.find(exon)
+    length = len(exon)
+
+    huge_coord = MAX_COORD - index
+    little_coord = MAX_COORD - (index + length - 1)
+
+    print(n, " | ", length, " | ", little_coord, " | ", huge_coord)
+    n += 1
