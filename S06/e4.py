c7c0afd7553ec911db2ea466b23991e0473046f7
raquelmargar <r.marting.2025@alumnos.urjc.es>
2026-02-25 16:57:06 +0100

    exercise 3 and 4

--- S06/e4.py
+++ S06/e4.py
@@ -12,14 +12,26 @@ class Seq:
         """Calculate the length of the sequence"""
         return len(self.strbases)
 
+def generate_seqs(pattern,n):
+    seq_list = []
+    for i in range(1,n+1):
+        seq_list.append(Seq(pattern * i ))
+    return seq_list
 
-def print_seqs(seq_list):
-    j = 0
-    for i in seq_list:
-        print(f"Sequence {j}: (Length: {i.len()}) {i}")
-        j += 1
+from termcolor import colored
 
-seq_list = [Seq("ACT"), Seq("GATA"), Seq("CAGATA")]
-print(print_seqs(seq_list))
+def print_seqs(seq_list, color):
+    for j, i in enumerate(seq_list):
+        text = f"Sequence {j}: (Length: {i.len()}) {i}"
+        print(colored(text, color))
 
+seq_list1 = (generate_seqs("A", 3))
+seq_list2 = (generate_seqs("AC", 5))
+
+print("List 1:")
+print_seqs(seq_list1,'magenta')
+
+print()
+print("List 2:")
+print_seqs(seq_list2,'yellow')
 
