1bfecad92ba6f3f80055773dc8f5087c97cc812e
raquelmargar <r.marting.2025@alumnos.urjc.es>
2026-03-03 16:04:45 +0100

    exercise 1 hacker

--- P01/e1.py
+++
@@ -1,6 +0,0 @@
-from Seq1 import Seq
-
-s1 = Seq("ACTGA")
-print("-----| Practice 1, Exercise 1 |------")
-
-print(f"Sequence 1: (Length: {s1.len()}): {s1}")
--- P01/e2.py
+++
@@ -1,7 +0,0 @@
-from Seq1 import Seq
-print("-----| Practice 1, Exercise 2 |------")
-s1 = Seq()
-s2 = Seq("ACTGTCGA")
-
-print(f"Sequence 1: {s1}")
-print(f"Sequence 2: {s2}")
