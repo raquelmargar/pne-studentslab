3908eabfe1765abcedd4281c28727e7dd81ef5e5
raquelmargar <r.marting.2025@alumnos.urjc.es>
2026-03-03 16:37:50 +0100

    exercise 3

---
+++ S08/e3.py
@@ -0,0 +1,13 @@
+import socket
+
+PORT = 8081
+IP = "212.128.255.64"
+
+while True:
+    ms = input("Enter your message:")
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    s.connect((IP, PORT))
+
+    s.send(str.encode(ms))
+    s.close()
