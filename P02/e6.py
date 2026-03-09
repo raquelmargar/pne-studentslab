bd5c5096da7c28f6c01d272cbebc703c0d87c5d3
raquelmargar <r.marting.2025@alumnos.urjc.es>
2026-03-09 20:32:12 +0100

    exercise 6

---
+++ P02/e6.py
@@ -0,0 +1,34 @@
+from Client0 import Client
+from Seq1 import Seq
+PRACTICE = 2
+EXERCISE = 6
+
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
+# -- Parameters of the server to talk to
+IP = "212.128.255.70" # your IP address
+PORT1 = 8080
+PORT2 = 8081
+
+# -- Create a client object
+c1 = Client(IP, PORT1)
+c2 = Client(IP, PORT2)
+
+s = Seq()
+gene = "FRAT1"
+rute = "../S04/sequences/" + gene + ".txt"
+base = str(s.read_fasta(rute))
+
+print(f"Gene {gene}")
+for i in range(0,10):
+
+    l = base[10*i : 10*(i+1)]
+
+    print(f"Fragment {i+1}: {l}")
+
+    if (i+1) % 2 != 0:
+        response = c1.talk(l)
+    else:
+        response = c2.talk(l)
+    print(f"To server: {l}")
+    print(f"From server: {response}")
