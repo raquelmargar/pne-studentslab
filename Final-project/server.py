39cfa747ac5476664dc5053eab7c82ccd0af7b25
raquelmargar <r.marting.2025@alumnos.urjc.es>
2026-05-19 11:38:11 +0200

    nivel medio terminado y super pro!!!!

--- Final-project/server.py
+++ Final-project/server.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from urllib.parse import parse_qs, urlparse
 import json
 import jinja2 as j
-
+from Seq1 import Seq
 PORT = 8080
 
 socketserver.TCPServer.allow_reuse_address = True
@@ -140,6 +140,160 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                         raise Exception()
                 else:
                     raise Exception()
+
+            elif path == "/geneLookup":
+                if "gene" in arguments:
+                    gene = arguments["gene"][0]
+                    conn = http.client.HTTPConnection("rest.ensembl.org")
+                    conn.request("GET", f"/lookup/symbol/homo_sapiens/{gene}?content-type=application/json")
+                    response = conn.getresponse()
+
+                    if response.status == 200:
+                        data = response.read().decode("utf-8")
+                        gene_data = json.loads(data)
+                        gene_id = gene_data.get("id")
+
+                        html_file = Path("html/lookup.html")
+                        contents = html_file.read_text()
+                        template = j.Template(contents)
+                        contents = template.render(gene=gene, id=gene_id)
+                        self.send_response(200)
+                    else:
+                        raise Exception()
+                else:
+                    raise Exception()
+
+            elif path == "/geneSeq":
+                if "gene" in arguments:
+                    gene = arguments["gene"][0]
+                    conn = http.client.HTTPConnection("rest.ensembl.org")
+                    conn.request("GET", f"/lookup/symbol/homo_sapiens/{gene}?content-type=application/json")
+                    response = conn.getresponse()
+
+                    if response.status == 200:
+                        gene_data = json.loads(response.read().decode("utf-8"))
+                        gene_id = gene_data.get("id")
+
+                        conn2 = http.client.HTTPConnection("rest.ensembl.org")
+                        conn2.request("GET", f"/sequence/id/{gene_id}?content-type=application/json")
+                        response2 = conn2.getresponse()
+
+                        if response2.status == 200:
+                            seq_data = json.loads(response2.read().decode("utf-8"))
+                            sequence = seq_data.get("seq")
+
+                            html_file = Path("html/Seq.html")
+                            contents = html_file.read_text()
+                            template = j.Template(contents)
+                            contents = template.render(gene=gene, sequence=sequence)
+                            self.send_response(200)
+                        else:
+                            raise Exception()
+                    else:
+                        raise Exception()
+
+
+            elif path == "/geneInfo":
+                if "gene" in arguments:
+                    gene = arguments["gene"][0]
+                    conn = http.client.HTTPConnection("rest.ensembl.org")
+                    conn.request("GET", f"/lookup/symbol/homo_sapiens/{gene}?content-type=application/json")
+                    response = conn.getresponse()
+
+                    if response.status == 200:
+                        gene_data = json.loads(response.read().decode("utf-8"))
+                        gene_id = gene_data.get("id")
+                        chromo = gene_data.get("seq_region_name")
+                        start = gene_data.get("start")
+                        end = gene_data.get("end")
+                        length = int(end) - int(start) + 1
+
+                        html_file = Path("html/info.html")
+                        contents = html_file.read_text()
+                        template = j.Template(contents)
+                        contents = template.render(gene=gene, chromo=chromo, start=start, end=end, length=length,
+                                                   id=gene_id)
+                        self.send_response(200)
+                    else:
+                        raise Exception()
+                else:
+                    raise Exception()
+
+
+            elif path == "/geneCalc":
+                if "gene" in arguments:
+                    gene = arguments["gene"][0]
+                    conn = http.client.HTTPConnection("rest.ensembl.org")
+                    conn.request("GET", f"/lookup/symbol/homo_sapiens/{gene}?content-type=application/json")
+                    response = conn.getresponse()
+
+                    if response.status == 200:
+                        gene_data = json.loads(response.read().decode("utf-8"))
+                        gene_id = gene_data.get("id")
+                        conn2 = http.client.HTTPConnection("rest.ensembl.org")
+                        conn2.request("GET", f"/sequence/id/{gene_id}?content-type=application/json")
+                        response2 = conn2.getresponse()
+
+                        if response2.status == 200:
+                            seq_data = json.loads(response2.read().decode("utf-8"))
+                            sequence = seq_data.get("seq")
+                            s = Seq(sequence)
+                            total_len = s.len()
+                            bases_count = s.count()
+
+                            if total_len > 0:
+                                perc_a = round((bases_count['A'] / total_len) * 100, 2)
+                                perc_c = round((bases_count['C'] / total_len) * 100, 2)
+                                perc_g = round((bases_count['G'] / total_len) * 100, 2)
+                                perc_t = round((bases_count['T'] / total_len) * 100, 2)
+
+                            else:
+                                perc_a = 0
+                                perc_c = 0
+                                perc_g = 0
+                                perc_t = 0
+
+                            html_file = Path("html/calculations.html")
+                            contents = html_file.read_text()
+                            template = j.Template(contents)
+                            contents = template.render(gene=gene, length=total_len, perc_A=perc_a, perc_C=perc_c,
+                                                       perc_G=perc_g, perc_T=perc_t)
+                            self.send_response(200)
+
+                        else:
+                            raise Exception()
+                    else:
+                        raise Exception()
+
+            elif path == "/geneList":
+                if "chromo" in arguments and "start" in arguments and "end" in arguments:
+                    chromo = arguments["chromo"][0]
+                    start = arguments["start"][0]
+                    end = arguments["end"][0]
+
+                    conn = http.client.HTTPConnection("rest.ensembl.org")
+                    conn.request("GET",
+                                 f"/overlap/region/homo_sapiens/{chromo}:{start}-{end}?feature=gene;content-type=application/json")
+                    response = conn.getresponse()
+
+                    if response.status == 200:
+                        overlap_data = json.loads(response.read().decode("utf-8"))
+                        genes_list = []
+
+                        for item in overlap_data:
+                            if "external_name" in item:
+                                genes_list.append(item["external_name"])
+
+                        html_file = Path("html/list.html")
+                        contents = html_file.read_text()
+                        template = j.Template(contents)
+                        contents = template.render(genes=genes_list, chromo=chromo, start=start, end=end)
+                        self.send_response(200)
+                    else:
+                        raise Exception()
+                else:
+                    raise Exception()
+
             else:
                 raise Exception()
 
